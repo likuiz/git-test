c1e950e2d75ecf7951013519ace70011a7caac0b
likui <likui@itcast.cn>
2019-07-31 10:24:03 +0800

    把本地工程添加到git

---
+++ git/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.itheima.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+
+    }
+}
